2a6fde9fefe40608f025606fe6e6f2f3528e5baa
Kevin Li <kevinli020508@gmail.com>
2023-09-27 15:33:35 -0400

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -4,14 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "apple_cantonese_asr_test",
-    dependencies: [
-        .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.1")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "apple_cantonese_asr_test", dependencies: ["SwiftCSV"]),
-    ]
+  name: "apple_cantonese_asr_test",
+  dependencies: [
+    .package(url: "https://github.com/swiftcsv/SwiftCSV.git", from: "0.8.1")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "apple_cantonese_asr_test", dependencies: ["SwiftCSV"])
+  ]
 )
